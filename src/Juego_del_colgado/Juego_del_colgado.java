b5c37c1b84d76a3eabf8d5a97415690d6ab0d61a
ruben1421 <rubenantoniomunoz0@gmail.com>
2024-11-18 16:00:00 +0100

    Numero de jugadores
    Indicamos las condiciones para el numero de jugadores

--- src/Juego_del_colgado/Juego_del_colgado.java
+++ src/Juego_del_colgado/Juego_del_colgado.java
@@ -3,29 +3,32 @@ package Juego_del_colgado;
 import java.util.Random;
 import java.util.Scanner;
 
+
 public class Juego_del_colgado {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Scanner x = new Scanner(System.in);
+		Scanner s = new Scanner(System.in);
 		
-		int intentos = 5;
+		System.out.print("INGRESE CUANTOS JUGADORES SE UNIRAN AL JUEGO : ");
+		int numjugadores = s. nextInt();
+		s.nextLine();
 		
-		int w = 0;
+		while(numjugadores < 2) 
+		{
+			if(numjugadores > 2) {
+				
+				break;
+			}
+			System.out.print("No se puede iniciar el juego con menos de dos jugadores ");
+			numjugadores = s. nextInt();
+			s.nextLine();
+		}
 		
-		boolean VD=true;
 		
-		int repetir = 0;
 		
-		System.out.println("escribe una palabra");
-		String palabra = x.nextLine();
 		
-		System.out.println("\n\n");
-		
-		String [] letras = palabra.split("");
-		
-		String intento [] = new String[letras.length];
 		
 		
 		
